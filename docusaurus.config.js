47e1dfe8ac19ed6ad4584348a75614a3b4c8610f
qiayuan <liaoqiayuan@gmail.com>
2021-08-28 17:28:41 +0800

    Replace logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,7 +31,7 @@ module.exports = {
       title: 'rm-controls Tutorial',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/logo.png',
       },
       items: [
         {
