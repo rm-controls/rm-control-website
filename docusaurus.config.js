778918750b3c8de779af9a68b1586e890261671a
qiayuan <liaoqiayuan@gmail.com>
2021-08-28 18:05:08 +0800

    Change img/logo.svg to img/logo.png

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,7 +32,7 @@ module.exports = {
       title: 'rm-controls Tutorial',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/logo.png',
       },
       items: [
         {
