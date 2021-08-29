9cb0bcc3913b630efbe58e75656b6b83c474e7ab
qiayuan <liaoqiayuan@gmail.com>
2021-08-29 18:43:12 +0800

    Fix baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'rm-controls documentation',
   tagline: '',
   url: 'https://rm-controls.github.io',
-  baseUrl: '/rm-controls-docs/',
+  baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
