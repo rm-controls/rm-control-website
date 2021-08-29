5fd5031afe02ae374fe6e0eb84ace02a5fda1662
MuZhou233 <muzhou233@outlook.com>
2021-08-29 18:05:50 +0800

    Update why content

--- src/theme/sections/why/why.js
+++ src/theme/sections/why/why.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import Heading from './../../components/heading/heading';
 import Container from './../../components/container/container';
+import WhyContext from '../../../../current_docs/overview/why_rm-controls.md'
 
 export default function Why() {
   const component = 'shift-why';
@@ -10,24 +9,9 @@ export default function Why() {
     <div className={component}>
       <Container
         componentClass={component}
-        size={'small'}
-      >
-        <Heading
-          componentClass={component}
-          title={'Why you would love to use this?'}
-          subtitle={'Easily use our pre-made 100% customizable existing Gutenberg blocks & components or create your own designs. Browse the collection of useful front-end utility modules and all the tools you need to start building modern Gutenberg dynamic blocks. There’s a bunch of helpers to write better code made by developers for developers. All code is written following the latest object-oriented programming paradigms.'}
-          align={'left'}
-          titleSize={'medium'}
-        />
-      </Container>
-      <Container
-        componentClass={component}
         size={'medium'}
       >
-        <div className={`${component}__content`}>
-          <img className={`${component}__image ${component}__image--desktop`} src={useBaseUrl('img/img-why-boilerplate@2x.png')} />
-          <img className={`${component}__image ${component}__image--mobile`} src={useBaseUrl('img/img-why-boilerplate-mobile@2x.png')} />
-        </div>
+        <WhyContext />
       </Container>
     </div>
   )
