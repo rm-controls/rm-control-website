baf8f4dd7c9dc117157dd2463ab7b699c73592d1
StarHeartHunt <starheart233@gmail.com>
2022-03-21 14:02:26 +0800

    fix: limit code scope to get `window.location.href` work

--- src/theme/sections/why/why.js
+++ src/theme/sections/why/why.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Container from "./../../components/container/container";
 import WhyContext from "../../../../current_docs/overview/why_rm-controls.md";
 import EnWhyContext from "../../../../i18n/en/docusaurus-plugin-content-docs/current/overview/why_rm-controls.md";
@@ -6,14 +6,16 @@ import EnWhyContext from "../../../../i18n/en/docusaurus-plugin-content-docs/cur
 export default function Why() {
   const component = "shift-why";
 
-  const [langCode, setLangCode] = useState(
-    window.location.href.indexOf("en/") != -1 ? "en" : "zh"
-  );
-
   return (
     <div className={component}>
       <Container componentClass={component} size={"medium"}>
-        {langCode == "en" ? <EnWhyContext /> : <WhyContext />}
+        <BrowserOnly>
+          {window.location.href.indexOf("en/") != -1 ? (
+            <EnWhyContext />
+          ) : (
+            <WhyContext />
+          )}
+        </BrowserOnly>
       </Container>
     </div>
   );
